f682e24147659ba0c078c8a5fb3ee08ecec5e0e6
Konrawee <konrawee207@gmail.com>
2023-03-09 15:49:28 +0700

    setup project

---
+++ lib/pages/SignInPage.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_app/animation/ScaleRoute.dart';
+import 'package:flutter_app/pages/FoodDetailsPage.dart';
+import 'package:flutter_app/pages/SignUpPage.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    String defaultFontFamily = 'Roboto-Light.ttf';
+    double defaultFontSize = 14;
+    double defaultIconSize = 17;
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(left: 20, right: 20, top: 35, bottom: 30),
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white70,
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: InkWell(
+                child: Container(
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Icon(Icons.close),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Flexible(
+              flex: 8,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 230,
+                    height: 100,
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      "assets/images/menus/logo.png",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextField(
+                    showCursor: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Color(0xFF666666),
+                        size: defaultIconSize,
+                      ),
+                      fillColor: Color(0xFFF2F3F5),
+                      hintStyle: TextStyle(
+                          color: Color(0xFF666666),
+                          fontFamily: defaultFontFamily,
+                          fontSize: defaultFontSize),
+                      hintText: "Email",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextField(
+                    showCursor: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                      prefixIcon: Icon(
+                        Icons.lock_outline,
+                        color: Color(0xFF666666),
+                        size: defaultIconSize,
+                      ),
+                      suffixIcon: Icon(
+                        Icons.remove_red_eye,
+                        color: Color(0xFF666666),
+                        size: defaultIconSize,
+                      ),
+                      fillColor: Color(0xFFF2F3F5),
+                      hintStyle: TextStyle(
+                        color: Color(0xFF666666),
+                        fontFamily: defaultFontFamily,
+                        fontSize: defaultFontSize,
+                      ),
+                      hintText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: Text(
+                      "Forgot your password?",
+                      style: TextStyle(
+                        color: Color(0xFF666666),
+                        fontFamily: defaultFontFamily,
+                        fontSize: defaultFontSize,
+                        fontStyle: FontStyle.normal,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  SignInButtonWidget(),
+                  SizedBox(
+                    height: 2,
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        "Don't have an account? ",
+                        style: TextStyle(
+                          color: Color(0xFF666666),
+                          fontFamily: defaultFontFamily,
+                          fontSize: defaultFontSize,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () => {
+                        Navigator.push(context, ScaleRoute(page: SignUpPage()))
+                      },
+                      child: Container(
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            color: Color(0xFFf7418c),
+                            fontFamily: defaultFontFamily,
+                            fontSize: defaultFontSize,
+                            fontStyle: FontStyle.normal,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignInButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Color(0xFFfbab66),
+          ),
+          BoxShadow(
+            color: Color(0xFFf7418c),
+          ),
+        ],
+        gradient: new LinearGradient(
+            colors: [Color(0xFFf7418c), Color(0xFFfbab66)],
+            begin: const FractionalOffset(0.2, 0.2),
+            end: const FractionalOffset(1.0, 1.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: MaterialButton(
+          highlightColor: Colors.transparent,
+          splashColor: Color(0xFFf7418c),
+          //shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 42.0),
+            child: Text(
+              "SIGN IN",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                  fontFamily: "WorkSansBold"),
+            ),
+          ),
+          onPressed: () => {}),
+    );
+  }
+}
