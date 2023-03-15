e20cba42232b455b14f9229191dd3f051341d1bf
กีตาร์เอาเอง <guitar@odds.team>
2023-03-15 14:44:28 +0700

    :guitar:

--- backend/controllers/menuController.js
+++ backend/controllers/menuController.js
@@ -2,6 +2,7 @@
 
 const firebase = require("../db");
 const firestore = firebase.firestore();
+const MenuResponse = require("../models/menu");
 
 const insertMenu = async (req, res, next) => {
   try {
@@ -15,6 +16,33 @@ const insertMenu = async (req, res, next) => {
   }
 };
 
+const getMenu = async (req, res, next) => {
+  try {
+    const menus = await firestore.collection("menus");
+    const data = await menus.get();
+    const menusArray = [];
+    if (data.empty) {
+      return res.status(404).json("No menu found");
+    } else {
+      data.forEach((doc) => {
+        const menu = new MenuResponse(
+          doc.id,
+          doc.data().name,
+          doc.data().price,
+          doc.data().description,
+          doc.data().image
+        );
+        menusArray.push(menu);
+      });
+      return res.status(200).json(menusArray);
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json("bad request");
+  }
+};
+
 module.exports = {
   InsertMenuService: insertMenu,
+  GetMenuService: getMenu,
 };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,6 +3,7 @@ const { RegisterService: Register } = require("../controllers/loginController");
 const { LoginService: Login } = require("../controllers/loginController");
 const {
   InsertMenuService: insertMenu,
+  GetMenuService: getMenu,
 } = require("../controllers/menuController");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("../swagger");
@@ -14,6 +15,7 @@ router.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 router.post("/register", Register);
 router.post("/login", Login);
 router.post("/menus", insertMenu);
+router.get("/menus", getMenu);
 
 module.exports = {
   routes: router,
